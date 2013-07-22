edb32d70b93ab623281ed74739ae08a6ce6503f4
iD Student <Student@S5-01-STRONGBAD.local>
2013-07-22 11:20:11 -0400

    auto commit

---
+++ Hello World/Hello World/IDDDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IDDDAppDelegate.h
+//  Hello World
+//
+//  Created by iD Student on 7/22/13.
+//  Copyright (c) 2013 iD Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IDDDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hello World/Hello World/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by iD Student on 7/22/13.
+//  Copyright (c) 2013 iD Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IDDDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IDDDAppDelegate class]));
+    }
+}
